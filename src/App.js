bb7935ddfe1f3f03f767a8269542a4e178585532
Erez Nagar <erez.nagar@gmail.com>
2020-05-21 11:51:21 -0400

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import FALLEN_SOLDIERS from "./fallenSoldiers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="app">
+    <header className="header">
+      <p>אלו האחים שלנו</p>
+    </header>
+    <div className="description" dir="rtl">
+      <p>
+        במשרד הביטחון קיימת רשימת נופלים רשמית לכל מערכות ישראל, מששת הימים ועד
+        לצוק איתן. לתקופת הלחימה בדרום לבנון אין. נאלצנו לספור אותם בעצמנו.
+      </p>
+      <p>
+        בימים אלו מציינים 20 שנים ליציאה מרצועת הביטחון בדרום לבנון. מדינת ישראל
+        לא מגדירה את התקופה הזאת, לא מחשיבה אותה כמלחמה ולא סופרת את חלליה.
+      </p>
+      <p>
+        675 נופלים בלבנון מיום סיום מלחמת שלום הגליל (מלחמת לבנון ה-1)
+        ב-29/9/1982 ועד ליום היציאה מרצועת הביטחון ב-24/5/2000.
+      </p>
+      <p>אלה האחים שלנו, ושוב כמו אז, אספנו אותם.</p>
     </div>
-  );
-}
+    <div className="listContainer">
+      <ul>
+        {FALLEN_SOLDIERS.map((soldier) => (
+          <li className="list-item">
+            {soldier.lastName} {soldier.firstName} &middot; בן {soldier.age}{" "}
+            &middot; נפל ב-{soldier.year}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <footer className="footer">
+      <p>יהי זכרם ברוך</p>
+      <p className="credit" dir="rtl">
+        מקור: מחקרו וספרו של חיים בר זהב, "לבנון: המלחמה האבודה"
+      </p>
+    </footer>
+  </div>
+);
 
 export default App;
