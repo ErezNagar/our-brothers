38e73ed2f2dc551a755811dfae6e35c8b8f76ea9
Erez Nagar <erez.nagar@gmail.com>
2020-06-12 14:55:33 -0400

    u

--- src/Press.js
+++ src/Press.js
@@ -1,8 +1,13 @@
 import React from "react";
 // import PropTypes from "prop-types";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Press = () => {
-  return <h2>Press</h2>;
+  return (
+    // <Router basename="/ppress">
+    <h2>Press inside /press</h2>
+    // </Router>
+  );
 };
 
 export default Press;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Press from "./Press";
 import * as serviceWorker from "./serviceWorker";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  HashRouter,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      {/* basename={"/"} */}
+      <Route exact path="/" component={App} />
+      <Route path="/press" component={Press} />
+    </HashRouter>
+    {/* <Router>
+      <Switch>
+        <Route path="/press">
+          <Press />
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root-press")
-// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
